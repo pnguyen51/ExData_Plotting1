7fbed3de6f57c945f649b24f910372198251e280
Phan Nguyen <phan.nguyen@u.northwestern.edu>
2015-01-09 01:20:50 -0600

    Plot 3/4 fixed.

--- plot3.R
+++ plot3.R
@@ -17,6 +17,6 @@ for(ii in 3:(ncol(temp)-1)){
 png('plot3.png')
 plot(temp$Time, temp$Sub_metering_1, type='l', col=1, xlab='', ylab='Energy sub metering')
 lines(temp$Time, temp$Sub_metering_2, col='red')
-lines(temp$Time, temp$Sub_metering_3, col='purple')
-legend('topright', legend=c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), col=c('black','red','purple'), lwd=1)
+lines(temp$Time, temp$Sub_metering_3, col='blue')
+legend('topright', legend=c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), col=c('black','red','blue'), lwd=1)
 dev.off()
